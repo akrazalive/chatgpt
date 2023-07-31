7fe393500d5fbfac86f8295385fa02668b95727b
Asif Khan <akrazalive@gmail.com>
2023-07-31 12:36:11 +0500

    Done some changes like moving code to  the root directory and some htaccess changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://rapidfiller.com/codeigniter/public/';
+    public string $baseURL = 'http://localhost/codeigniter/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
